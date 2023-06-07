e54d6b439f1589cb3eef4c377e225a24529ff50d
Eugenio Caruso <ecaruso@student.42roma.it>
2023-06-07 17:10:26 +0200

    hello

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 18:39:04 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/06/03 17:49:24 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/06/07 16:57:24 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,20 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc > 1)
+	t_stack	*a;
+	t_stack	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+	//push_swap(argv);
+	while (!argv)
 	{
+		ft_printf("%d", argv);
 		argv++;
-		if (argc == 2)
-			argv = ft_split(*argv, ' ');
-		push_swap(argv);
-		return (0);
 	}
 	return (0);
-}
+	}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 18:13:51 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/06/03 17:57:54 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/06/07 17:02:44 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include "./ft_printf/ft_printf.h"
 # include "./libft/libft.h"
+
+typedef struct s_stack
+{
+	struct s_stack *next;
+	struct s_stack *prev;
+}			t_stack;
 
 int	main(int argc, char **argv);
 
