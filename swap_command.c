10d295c77a02108666500da6f07c310fb905c857
Eugenio Caruso <ecaruso@student.42roma.it>
2023-06-25 14:44:47 +0200

    hello

--- swap_command.c
+++ swap_command.c
@@ -6,7 +6,7 @@
 /*   By: ecaruso <ecaruso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 19:11:07 by ecaruso           #+#    #+#             */
-/*   Updated: 2023/06/19 19:13:24 by ecaruso          ###   ########.fr       */
+/*   Updated: 2023/06/25 14:44:31 by ecaruso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@ static void	swap(t_stack_node **head)
 	len = stack_len(*head);
 	if (NULL == *head || NULL == head || 1 == len)
 		return ;
-
+	*head = (*head)->next;
+	(*head)->prev->prev = *head;
+	(*head)->prev->next = (*head)->next;
+	if ((*head)->next)
+		(*head)->next->prev = (*head)->prev;
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
 }
